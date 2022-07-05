f343554a18c48faa280f8edfe657c2e4e0139c0d
JohnTelman <>
2022-07-05 11:37:46 +0200

    terrain-visual-working

--- src/client/three/ViewGL.tsx
+++ src/client/three/ViewGL.tsx
@@ -5,11 +5,12 @@ import Stats from "three/examples/jsm/libs/stats.module";
 export default class ViewGL {
   private scene: THREE.Scene;
   private renderer: THREE.WebGLRenderer;
-  private mapWidth: number;
-  private mapLength: number;
+  //private mapWidth: number;
+  //private mapLength: number;
 
   // Camera
   private camera: THREE.PerspectiveCamera;
+  private camX: number;
   private camY: number;
   private camZ: number;
 
@@ -17,10 +18,8 @@ export default class ViewGL {
   private controls: any;
   private stats: any;
 
-  constructor(canvasRef: any) {
-    this.mapLength = 1000;
-    this.mapWidth = 1000;
-
+  constructor(canvasRef: any)
+  {
     // CREATE SCENE AND RENDERER
     this.scene = new THREE.Scene();
 
@@ -42,10 +41,11 @@ export default class ViewGL {
     this.scene.add(light);
 
     // CAMERA
-    this.camY = 85;
-    this.camZ = 65;
+    this.camX = 0
+    this.camY = 10;
+    this.camZ = 0;
     this.camera = new THREE.PerspectiveCamera(
-      20,
+      45,
       window.innerWidth / window.innerHeight,
       1,
       1000
@@ -54,8 +54,41 @@ export default class ViewGL {
     this.camera.position.z = this.camZ;
     this.controls = new OrbitControls(this.camera, canvasRef);
 
+    // ******************* GET WORLD READY *******************//
+    this.terrainCreate();
+
+
     // CALL ANIMATION LOOP
     this.update();
+  }
+  // ****************** FUNCTIONS ********************** //
+
+  terrainCreate = () =>
+  {
+    var terrain: THREE.Mesh;
+    let terrainPlane = new THREE.PlaneGeometry(40, 16, 1, 1);
+    terrainPlane.rotateX(-Math.PI * 0.5);
+    const texture = new THREE.TextureLoader().load('resources/textures/Grass_Gen1.png');
+
+    let mat = new THREE.MeshStandardMaterial(
+    {
+      map: texture,
+      transparent: true,
+      depthWrite: false,
+      depthTest: true,
+      // shading: 2
+    });
+
+    if(mat.map)
+    {
+        mat.map.repeat = new THREE.Vector2(1, 1); // TEXTURE TILLING
+        mat.map.wrapS = THREE.RepeatWrapping; // REPEAT X
+        mat.map.wrapT = THREE.RepeatWrapping; // REPEAT Y
+        mat.map.magFilter = THREE.NearestFilter; // NEAREST/LINEAR FILTER LinearFilter NearestFilter
+    }
+
+    terrain = new THREE.Mesh(terrainPlane, mat);
+    this.scene.add(terrain);
   }
 
   // ******************* PUBLIC EVENTS ******************* //
@@ -65,8 +98,16 @@ export default class ViewGL {
     this.renderer.setSize(vpW, vpH);
   };
 
+
   // ******************* RENDER LOOP ******************* //
   update = (t?: any) => {
+
+
+
+    // cam
+    this.controls.target = new THREE.Vector3(0, 0, 0);
+
+
     this.controls.update();
 
     // this.stats.update();
