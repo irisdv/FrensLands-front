a9dd16c8a51b1964bafee9bf479818f2ebe67d9b
Iris <iris.devillars@gmail.com>
2022-07-28 14:49:05 +0200

    add broken cabin

--- src/client/components/r3f/ResourceItem.tsx
+++ src/client/components/r3f/ResourceItem.tsx
@@ -60,6 +60,8 @@ export const ResourceItem = memo<IBlock>(({block, textArrRef, rightBuildingType,
                     textureType = findTextByID(30);
                 }
             }
+        } else if (block[3] == 1 && block[7] == 1) {
+            textureType = findTextByID(2)
         } else {
             textureType = findTextByID(rightBuildingType[block[3]]);
         }
