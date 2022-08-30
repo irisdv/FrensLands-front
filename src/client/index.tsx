3aaa1e041d9e27ecbe79a018f98637e09a624385
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-08-31 01:27:10 +0200

    install @starknet/burner in frenslands

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { getInstalledInjectedConnectors, InjectedConnector, StarknetProvider } from "@starknet-react/core";
+import {
+  getInstalledInjectedConnectors,
+  InjectedConnector,
+  StarknetProvider,
+} from "@starknet-react/core";
 import { BrowserRouter } from "react-router-dom";
 import { AppStateProvider } from "./providers/GameContext";
-import { NotifTransactionManagerProvider } from './providers/transactions'
-
+import { NotifTransactionManagerProvider } from "./providers/transactions";
+import { keyManager } from "@starknet/burner";
 const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 // const connectors = getInstalledInjectedConnectors();
 
 const connectors = [
-  new InjectedConnector({ options: { id: 'argent-x' } }),
-  new InjectedConnector({ options: { id: 'braavos' } }),
-]
+  new InjectedConnector({ options: { id: "589c80c1eb85413d" } }),
+  new InjectedConnector({ options: { id: "argent-x" } }),
+  new InjectedConnector({ options: { id: "braavos" } }),
+];
 
-
+keyManager();
 
 root.render(
   <>
@@ -23,10 +28,10 @@ root.render(
       <NotifTransactionManagerProvider>
         <BrowserRouter>
           <AppStateProvider>
-              <App />
+            <App />
           </AppStateProvider>
         </BrowserRouter>
       </NotifTransactionManagerProvider>
     </StarknetProvider>
   </>
-); 
+);
